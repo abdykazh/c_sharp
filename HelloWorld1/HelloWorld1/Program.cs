805012233ce77b27efceb6bbccc447c08d2fc15e
abdykazh <zhibek.abdykalykova@student.uni-augsburg.de>
2021-12-03 17:08:50 +0100

    c# primitive types

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+byte number = 1;
+int count = 10;
+float total = 20.95f;
+char ch = 'A';
+string name = "Zhibek";
+bool isWorking = true;
+var s = "f";
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(number);
+Console.WriteLine(count);
+
+Console.WriteLine("{0}", byte.MaxValue);
+
+
+byte b = 1;
+int i = b;
+// no data loss
+Console.WriteLine(i);
+
+int j = 1000;
+byte a = (byte)i;
+// cast, expicit conversion, u are aware of data loss
+// data types should be compatible
+Console.WriteLine(a);
+
+try
+{
+    var num = "2";
+    byte k = Convert.ToByte(num);
+    Console.WriteLine(k);
+}
+catch (Exception)
+{
+    throw;
+}
+
+a = 2;
+b = 3;
+var c = 2;
+Console.WriteLine((float)a/b);
+
+Console.WriteLine(a + b*c);
